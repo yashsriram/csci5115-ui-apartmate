c7a28688373397698b4ae341f1aeba432324eeb3
Yashasvi Sriram <yash.3997@gmail.com>
2019-11-08 18:38:06 +0530

    impls basic feature toggling functionality in AptSearchResultLayoutCustomization activity

--- app/src/main/java/com/example/group8project/AptSRLCustomizationActivity.java
+++ app/src/main/java/com/example/group8project/AptSRLCustomizationActivity.java
@@ -1,14 +1,27 @@
 package com.example.group8project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AptSRLCustomizationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apt_srlcustomization);
+        setContentView(R.layout.activity_apt_srl_customization);
+
+        final Switch switch1 = findViewById(R.id.switch1);
+        final TextView feature1 = findViewById(R.id.feature1);
+        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                feature1.setVisibility(b ? View.VISIBLE : View.GONE);
+            }
+        });
     }
 }
