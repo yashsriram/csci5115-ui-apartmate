4b5761401f32bfca43a5fdaf2dcae9e360b8d02e
Yashasvi Sriram <yash.3997@gmail.com>
2019-11-09 00:11:22 +0530

    adds Create Apartment Listing activity and links it to remaining activities

---
+++ app/src/main/java/com/csci5115/group8/CreateApartmentListing.java
@@ -0,0 +1,30 @@
+package com.csci5115.group8;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class CreateApartmentListing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_apartment_listing);
+
+        final ConstraintLayout layout = findViewById(R.id.activity_create_apartment_listing);
+        FloatingActionButton submit = layout.findViewById(R.id.submit_new_apartment_listing);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Replace with your own action", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/csci5115/group8/ui/aptsearch/AptSearchFragment.java
+++ app/src/main/java/com/csci5115/group8/ui/aptsearch/AptSearchFragment.java
@@ -1,5 +1,6 @@
 package com.csci5115.group8.ui.aptsearch;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.csci5115.group8.CreateApartmentListing;
 import com.csci5115.group8.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -35,7 +37,8 @@ public class AptSearchFragment extends Fragment {
         createAptListing.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                Intent intent = new Intent(getContext(), CreateApartmentListing.class);
+                startActivity(intent);
             }
         });
         final FloatingActionButton searchApartments = root.findViewById(R.id.search_apartments);
