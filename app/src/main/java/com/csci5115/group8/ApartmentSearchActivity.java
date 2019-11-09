1bad21c81050334d98b16935bab4e842463ca4be
Yashasvi Sriram <yash.3997@gmail.com>
2019-11-09 16:31:29 +0530

    adds UI in apartment search activity

--- app/src/main/java/com/csci5115/group8/ApartmentSearchActivity.java
+++ app/src/main/java/com/csci5115/group8/ApartmentSearchActivity.java
@@ -9,6 +9,8 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import it.beppi.tristatetogglebutton_library.TriStateToggleButton;
+
 public class ApartmentSearchActivity extends AppCompatActivity {
 
     @Override
@@ -23,6 +25,24 @@ public class ApartmentSearchActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(getApplicationContext(), "Replace with your own action", Toast.LENGTH_LONG).show();
                 finish();
+            }
+        });
+
+        TriStateToggleButton tstb_1 = findViewById(R.id.buildingLock);
+        tstb_1.setOnToggleChanged(new TriStateToggleButton.OnToggleChanged() {
+            @Override
+            public void onToggle(TriStateToggleButton.ToggleStatus toggleStatus, boolean booleanToggleStatus, int toggleIntValue) {
+                switch (toggleStatus) {
+                    case off:
+                        Toast.makeText(ApartmentSearchActivity.this, "off", Toast.LENGTH_SHORT).show();
+                        break;
+                    case mid:
+                        Toast.makeText(ApartmentSearchActivity.this, "mid", Toast.LENGTH_SHORT).show();
+                        break;
+                    case on:
+                        Toast.makeText(ApartmentSearchActivity.this, "on", Toast.LENGTH_SHORT).show();
+                        break;
+                }
             }
         });
     }
