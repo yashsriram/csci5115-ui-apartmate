b469ab9460684be64619fb3be0aaaa997ec25657
Tom Juszczyk <tomkj4@gmail.com>
2019-11-09 16:41:09 -0600

    chat progress

--- app/src/main/java/com/csci5115/group8/ThreadActivity.java
+++ app/src/main/java/com/csci5115/group8/ThreadActivity.java
@@ -1,17 +1,26 @@
 package com.csci5115.group8;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.csci5115.group8.data.DataManager;
 import com.csci5115.group8.data.Thread;
+import com.csci5115.group8.ui.chatThread.ChatThreadFragment;
+import com.csci5115.group8.ui.dummy.DummyContent;
+import com.csci5115.group8.ui.threadMessage.threadMessagesFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 
-public class ThreadActivity extends AppCompatActivity {
+public class ThreadActivity extends AppCompatActivity implements threadMessagesFragment.OnListFragmentInteractionListener {
 
     private Thread thread;
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +39,7 @@ public class ThreadActivity extends AppCompatActivity {
         }
         System.out.println("GOT");
         System.out.println(this.thread.first_name);
+        setTitle("Chat - " + this.thread.first_name + " " + this.thread.last_name);
     }
 
 }
