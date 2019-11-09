fbc249ec5884ee92c8e6449e039a0e95cd21045e
Yashasvi Sriram <yash.3997@gmail.com>
2019-11-09 16:56:23 +0530

    adds some backend code and improves ui in apt search activity

--- app/src/main/java/com/csci5115/group8/ApartmentSearchActivity.java
+++ app/src/main/java/com/csci5115/group8/ApartmentSearchActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import it.beppi.tristatetogglebutton_library.TriStateToggleButton;
 
@@ -19,30 +23,52 @@ public class ApartmentSearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_apartment_search);
 
         final ConstraintLayout layout = findViewById(R.id.activity_apartment_search);
+
+        final EditText searchText = layout.findViewById(R.id.searchText);
+        final TextView numMatches = layout.findViewById(R.id.numMatches);
+        searchText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View view, int i, KeyEvent keyEvent) {
+                // Get search string
+                String string = searchText.getText().toString();
+                // Show number of matches using regex matching across all apartments
+                return false;
+            }
+        });
+
+        final TriStateToggleButton refrigerator = layout.findViewById(R.id.refrigerator);
+        final TriStateToggleButton oven = layout.findViewById(R.id.oven);
+        final TriStateToggleButton microwave = layout.findViewById(R.id.microwave);
+        final TriStateToggleButton dishwasher = layout.findViewById(R.id.dishwasher);
+        final TriStateToggleButton washingMachine = layout.findViewById(R.id.washingMachine);
+        final TriStateToggleButton heating = layout.findViewById(R.id.heating);
+        final TriStateToggleButton cooling = layout.findViewById(R.id.cooling);
+
+        final TriStateToggleButton laundryRoom = layout.findViewById(R.id.laundryRoom);
+        final TriStateToggleButton longue = layout.findViewById(R.id.longue);
+        final TriStateToggleButton printingService = layout.findViewById(R.id.printingService);
+        final TriStateToggleButton reception = layout.findViewById(R.id.reception);
+        final TriStateToggleButton parking = layout.findViewById(R.id.parking);
+
+        final TriStateToggleButton securityCameras = layout.findViewById(R.id.securityCameras);
+        final TriStateToggleButton smokeDetectors = layout.findViewById(R.id.smokeDetectors);
+        final TriStateToggleButton sprinklers = layout.findViewById(R.id.sprinklers);
+        final TriStateToggleButton buildingLock = layout.findViewById(R.id.buildingLock);
+
         FloatingActionButton submit = layout.findViewById(R.id.submit_apartment_search);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Replace with your own action", Toast.LENGTH_LONG).show();
-                finish();
-            }
-        });
-
-        TriStateToggleButton tstb_1 = findViewById(R.id.buildingLock);
-        tstb_1.setOnToggleChanged(new TriStateToggleButton.OnToggleChanged() {
-            @Override
-            public void onToggle(TriStateToggleButton.ToggleStatus toggleStatus, boolean booleanToggleStatus, int toggleIntValue) {
-                switch (toggleStatus) {
-                    case off:
-                        Toast.makeText(ApartmentSearchActivity.this, "off", Toast.LENGTH_SHORT).show();
-                        break;
-                    case mid:
-                        Toast.makeText(ApartmentSearchActivity.this, "mid", Toast.LENGTH_SHORT).show();
-                        break;
-                    case on:
-                        Toast.makeText(ApartmentSearchActivity.this, "on", Toast.LENGTH_SHORT).show();
-                        break;
+                // Validate
+                if (searchText.getText().toString().isEmpty()) {
+                    Snackbar.make(view, "Please enter something in search bar", Snackbar.LENGTH_LONG).show();
+                    return;
                 }
+                // Collect data
+                // Serialize
+                // Put in intent
+                // Finish
+                finish();
             }
         });
     }
