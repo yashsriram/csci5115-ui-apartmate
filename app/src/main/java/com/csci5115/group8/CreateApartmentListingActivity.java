584b1cdb08fa13307e801cc13c38539eff147ea9
Yashasvi Sriram <yash.3997@gmail.com>
2019-11-10 14:16:44 +0530

    adds create unit in apt form UI in create apt listing activity, improves ui

--- app/src/main/java/com/csci5115/group8/CreateApartmentListingActivity.java
+++ app/src/main/java/com/csci5115/group8/CreateApartmentListingActivity.java
@@ -1,7 +1,9 @@
 package com.csci5115.group8;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +11,23 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class CreateApartmentListingActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreateApartmentListingActivity extends ListActivity {
+    List<String> listItems = new ArrayList<>();
+
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_apartment_listing);
+
+        listItems.add("LOL");
+        listItems.add("Foo");
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listItems);
+        setListAdapter(adapter);
 
         final ConstraintLayout layout = findViewById(R.id.activity_create_apartment_listing);
         FloatingActionButton submit = layout.findViewById(R.id.submit_new_apartment_listing);
