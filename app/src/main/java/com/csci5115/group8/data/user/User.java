3cf771d8ca6972a9cea069d31d02f9cf2f42aba1
Fatima Rahman <rahma176@umn.edu>
2019-11-11 15:34:46 -0600

    adds fields (age, location, etc) to user class

--- app/src/main/java/com/csci5115/group8/data/user/User.java
+++ app/src/main/java/com/csci5115/group8/data/user/User.java
@@ -4,4 +4,14 @@ public class User {
     public String name;
     public String gender;
     public String age;
+    public String currentLocation;
+    public int maxBudget;
+    public boolean doesSmoke;
+    public boolean drugsOkay;
+    public boolean hasPets;
+    public boolean partiesOkay;
+    public boolean canCook;
+    public boolean needsPrivateBedroom;
+    public boolean nativeLanguage;
+    public boolean hasCar;
 }
