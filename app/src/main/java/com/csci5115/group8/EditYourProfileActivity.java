4cdeaa1844aaeb7cc32f5a1a84768892a81d404a
Fatima Rahman <rahma176@umn.edu>
2019-11-10 23:20:15 -0600

    Update EditYourProfileActivity.java

--- app/src/main/java/com/csci5115/group8/EditYourProfileActivity.java
+++ app/src/main/java/com/csci5115/group8/EditYourProfileActivity.java
@@ -11,19 +11,92 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EditYourProfileActivity extends AppCompatActivity {
 
+   String firstName, lastName, city;
+   int age;
+
+   //private static final int RESULT_LOAD_IMAGE;
+   EditText firstNameInput, lastNameInput, ageInput, cityInput;
+   Button submit;
+   ImageView imageToUpload;
+   Button uploadImage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_your_profile);
+        setContentView(R.layout.activity_main);
 
-        final ConstraintLayout layout = findViewById(R.id.activity_edit_your_profile);
-        FloatingActionButton submit = layout.findViewById(R.id.submit_edited_user_profile);
+        firstNameInput = (EditText) findViewById(R.id.firstNameInput);
+        lastNameInput = (EditText) findViewById(R.id.surnameInput);
+        ageInput = (EditText) findViewById(R.id.ageInput);
+        cityInput = (EditText) findViewById(R.id.cityInput);
+
+        imageToUpload = (ImageView) findViewById(R.id.imageToUpload);
+        uploadImage = (Button) findViewById(R.id.uploadImage);
+
+        submit = (Button) findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Replace with your own action", Toast.LENGTH_LONG).show();
-                finish();
+            public void onClick(View v) {
+                firstName = firstNameInput.getText().toString();
+                lastName = lastNameInput.getText().toString();
+                age = Integer.valueOf(ageInput.getText().toString());
+                city = cityInput.getText().toString();
+                /*
+                switch (v.getId()) {
+                    case R.id.imageToUpload:
+                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+                        break;
+                    case R.id.uploadImage:
+                        Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
+                        new UploadImage(image).execute();
+                        break;
+                }
+               */
             }
+
         });
+
+        /*
+        private void showToast (String text){
+            Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+        }
+
+        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
+
+            super.onActivityResult(requestCode, resultCode, data);
+            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+                Uri selectedImage = data.getData();
+                imageToUpload.setImageURI(selectedImage);
+            }
+
+        }
+
+        public class UploadImage extends AsyncTask<Void, Void, Void> {
+            Bitmap image;
+
+            public UploadImage(Bitmap image) {
+                this.image = image;
+            }
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+                image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+                String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Toast.makeText(getApplicationContext(), "Image Uploaded", Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        */
+
+
     }
 }
