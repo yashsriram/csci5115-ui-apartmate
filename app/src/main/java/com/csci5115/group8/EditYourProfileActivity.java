896432c63f6ecf4b5a37b278d86456ae25ecef91
Fatima Rahman <rahma176@umn.edu>
2019-11-15 13:08:31 -0600

    accounts for new variables in UserPreferences

--- app/src/main/java/com/csci5115/group8/EditYourProfileActivity.java
+++ app/src/main/java/com/csci5115/group8/EditYourProfileActivity.java
@@ -3,21 +3,34 @@ package com.csci5115.group8;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import com.csci5115.group8.data.apartment.Apartment;
+import com.csci5115.group8.data.apartment.ApartmentUnit;
+import com.csci5115.group8.data.apartment.CommonAmenities;
+import com.csci5115.group8.data.DataManager;
+import com.csci5115.group8.data.apartment.PerUnitAmenities;
+import com.csci5115.group8.data.apartment.SecurityFeatures;
+
 public class EditYourProfileActivity extends AppCompatActivity {
 
-    String firstName, lastName, city, college;
-    int age;
+    String firstName, lastName, email, age, city;
+    int maxBudget;
+    boolean pet, car, bedroom, party, drug, smoke;
 
     //private static final int RESULT_LOAD_IMAGE;
-    EditText firstNameInput, lastNameInput, ageInput, cityInput, collegeInput;
+    EditText firstNameInput, lastNameInput, emailInput, ageInput, cityInput, maxBudgetInput;
     Button submit;
     ImageView imageToUpload;
     Button uploadImage;
+    CheckBox petInput, carInput, bedroomInput, partyInput, drugInput, smokeInput;
 
 
     @Override
@@ -26,10 +39,17 @@ public class EditYourProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_your_profile);
 
         firstNameInput = findViewById(R.id.firstNameInput);
-        lastNameInput = findViewById(R.id.surnameInput);
+        lastNameInput = findViewById(R.id.lastNameInput);
+        emailInput = findViewById(R.id.emailInput);
         ageInput = findViewById(R.id.ageInput);
         cityInput = findViewById(R.id.cityInput);
-        collegeInput = findViewById(R.id.collegeInput);
+        maxBudgetInput = findViewById(R.id.maxBudgetInput);
+        petInput = findViewById(R.id.petInput);
+        carInput = findViewById(R.id.carInput);
+        bedroomInput = findViewById(R.id.bedroomInput);
+        partyInput = findViewById(R.id.partyInput);
+        drugInput = findViewById(R.id.drugInput);
+        smokeInput = findViewById(R.id.smokeInput);
 
         imageToUpload = findViewById(R.id.imageToUpload);
         uploadImage = findViewById(R.id.uploadImage);
@@ -40,9 +60,18 @@ public class EditYourProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 firstName = firstNameInput.getText().toString();
                 lastName = lastNameInput.getText().toString();
-                age = Integer.valueOf(ageInput.getText().toString());
+                email = emailInput.getText().toString();
+                age = ageInput.getText().toString();
                 city = cityInput.getText().toString();
-                college = collegeInput.getText().toString();
+                maxBudget = Integer.parseInt(maxBudgetInput.getText().toString());
+                pet = petInput.isChecked();
+                car = carInput.isChecked();
+                bedroom = bedroomInput.isChecked();
+                party = partyInput.isChecked();
+                drug = drugInput.isChecked();
+                smoke = smokeInput.isChecked();
+                // Go back
+                finish();
                 /*
                 switch (v.getId()) {
                     case R.id.imageToUpload:
