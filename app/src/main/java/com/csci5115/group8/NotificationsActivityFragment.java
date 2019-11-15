c66aa8e94a8887019f63722a37cf95b2b0495868
Tom Juszczyk <tomkj4@gmail.com>
2019-11-15 12:09:06 -0600

    notifications page initial

---
+++ app/src/main/java/com/csci5115/group8/NotificationsActivityFragment.java
@@ -0,0 +1,23 @@
+package com.csci5115.group8;
+
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class NotificationsActivityFragment extends Fragment {
+
+    public NotificationsActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+    }
+}
