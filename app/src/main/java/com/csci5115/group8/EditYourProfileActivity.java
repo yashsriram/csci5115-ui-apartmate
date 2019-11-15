f0b5715d2a9a8624389a9b3f035d820fc5663801
Yashasvi Sriram <yash.3997@gmail.com>
2019-11-15 18:28:06 +0530

    init edit your profile with existing data

--- app/src/main/java/com/csci5115/group8/EditYourProfileActivity.java
+++ app/src/main/java/com/csci5115/group8/EditYourProfileActivity.java
@@ -1,24 +1,18 @@
 package com.csci5115.group8;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import com.csci5115.group8.data.apartment.Apartment;
-import com.csci5115.group8.data.apartment.ApartmentUnit;
-import com.csci5115.group8.data.apartment.CommonAmenities;
 import com.csci5115.group8.data.DataManager;
-import com.csci5115.group8.data.apartment.PerUnitAmenities;
-import com.csci5115.group8.data.apartment.SecurityFeatures;
+import com.csci5115.group8.data.user.User;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class EditYourProfileActivity extends AppCompatActivity {
 
@@ -28,103 +22,50 @@ public class EditYourProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_your_profile);
 
+        User currentUser = DataManager.currentUser;
+        final TextView isVerified = findViewById(R.id.isVerified);
+        final EditText name = findViewById(R.id.name);
+        final EditText email = findViewById(R.id.email);
+        final EditText age = findViewById(R.id.age);
+        final EditText maxBudget = findViewById(R.id.maxBudget);
+        final EditText gender = findViewById(R.id.gender);
+        final EditText nativeLanguage = findViewById(R.id.nativeLanguage);
+        final CheckBox doesSmoke = findViewById(R.id.doesSmoke);
+        final CheckBox drugsOkay = findViewById(R.id.drugsOkay);
+        final CheckBox hasPets = findViewById(R.id.hasPets);
+        final CheckBox partiesOkay = findViewById(R.id.partiesOkay);
+        final CheckBox canCook = findViewById(R.id.canCook);
+        final CheckBox hasCar = findViewById(R.id.hasCar);
+        final CheckBox needsPrivateBedroom = findViewById(R.id.needsPrivateBedroom);
+        final ImageView image = findViewById(R.id.image);
 
-        String firstName, lastName, email, age, city;
-        int maxBudget;
-        boolean pet, car, bedroom, party, drug, smoke;
+        isVerified.setText(currentUser.isVerified ? "VERIFIED" : "NOT VERIFIED");
+        isVerified.setTextColor(currentUser.isVerified ? Color.GREEN : Color.RED);
+        name.setText(currentUser.name);
+        email.setText(currentUser.email);
+        age.setText(String.format("%s", currentUser.age));
+        maxBudget.setText(String.format("%s", currentUser.maxBudget));
+        gender.setText(currentUser.gender);
+        nativeLanguage.setText(currentUser.nativeLanguage);
+        doesSmoke.setChecked(currentUser.preferences.doesSmoke);
+        drugsOkay.setChecked(currentUser.preferences.drugsOkay);
+        hasPets.setChecked(currentUser.preferences.hasPets);
+        partiesOkay.setChecked(currentUser.preferences.partiesOkay);
+        canCook.setChecked(currentUser.preferences.canCook);
+        needsPrivateBedroom.setChecked(currentUser.preferences.needsPrivateBedroom);
+        hasCar.setChecked(currentUser.preferences.hasCar);
 
-        EditText nameInput, lastNameInput, emailInput, ageInput, cityInput, maxBudgetInput;
-        FloatingActionButton submit;
-        ImageView imageToUpload;
-        Button uploadImage;
-        CheckBox petInput, carInput, bedroomInput, partyInput, drugInput, smokeInput;
+        FloatingActionButton submit = findViewById(R.id.submit);
 
-        nameInput = findViewById(R.id.nameInput);
-        emailInput = findViewById(R.id.emailInput);
-        ageInput = findViewById(R.id.ageInput);
-        maxBudgetInput = findViewById(R.id.maxBudgetInput);
-        petInput = findViewById(R.id.petInput);
-        carInput = findViewById(R.id.carInput);
-        bedroomInput = findViewById(R.id.bedroomInput);
-        partyInput = findViewById(R.id.partyInput);
-        drugInput = findViewById(R.id.drugInput);
-        smokeInput = findViewById(R.id.smokeInput);
-
-        imageToUpload = findViewById(R.id.imageToUpload);
-
-        submit = findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                firstName = nameInput.getText().toString();
-//                lastName = lastNameInput.getText().toString();
-//                email = emailInput.getText().toString();
-//                age = ageInput.getText().toString();
-//                city = cityInput.getText().toString();
-//                maxBudget = Integer.parseInt(maxBudgetInput.getText().toString());
-//                pet = petInput.isChecked();
-//                car = carInput.isChecked();
-//                bedroom = bedroomInput.isChecked();
-//                party = partyInput.isChecked();
-//                drug = drugInput.isChecked();
-//                smoke = smokeInput.isChecked();
-                // Go back
+                // Get fields
+                // Validate (all fields filled, email should not be duplicate)
+                // Save user
                 finish();
-                /*
-                switch (v.getId()) {
-                    case R.id.imageToUpload:
-                        Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
-                        break;
-                    case R.id.uploadImage:
-                        Bitmap image = ((BitmapDrawable) imageToUpload.getDrawable()).getBitmap();
-                        new UploadImage(image).execute();
-                        break;
-                }
-               */
             }
 
         });
-
-        /*
-        private void showToast (String text){
-            Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
-        }
-
-        protected void onActivityResult ( int requestCode, int resultCode, Intent data){
-
-            super.onActivityResult(requestCode, resultCode, data);
-            if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
-                Uri selectedImage = data.getData();
-                imageToUpload.setImageURI(selectedImage);
-            }
-
-        }
-
-        public class UploadImage extends AsyncTask<Void, Void, Void> {
-            Bitmap image;
-
-            public UploadImage(Bitmap image) {
-                this.image = image;
-            }
-
-            @Override
-            protected Void doInBackground(Void... voids) {
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-                image.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
-                String encodedImage = Base64.encodeToString(byteArrayOutputStream.toByteArray(), Base64.DEFAULT);
-                return null;
-            }
-
-            @Override
-            protected void onPostExecute(Void aVoid) {
-                super.onPostExecute(aVoid);
-                Toast.makeText(getApplicationContext(), "Image Uploaded", Toast.LENGTH_SHORT).show();
-            }
-
-        }
-        */
-
-
     }
 }
